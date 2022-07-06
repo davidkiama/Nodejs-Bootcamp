6082675d7573b7c3e8c1542f00edddd7ffddc5ed
davidkiama <davidkiama98@gmail.com>
2022-07-06 21:51:43 +0300

    Basic web server

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
-const fs = require("fs");
+const http = require("http");
 
-//Blocking, sycnhronous way
-// const textInput = fs.readFileSync("./txt/input.txt", "utf-8");
-// const textOutput = `This is what we know about the avocado: ${textInput}.\nCreated on date ${Date.now()}`;
-// console.log(textInput);
-// fs.writeFileSync("./txt/output.txt", textOutput);
-// console.log("File has been written.");
-
-//Non-blocking, asycnhronous way
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  if (err) return console.log("Error");
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
-    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-      console.log(data3);
-      fs.writeFile("./txt/writtenAsync.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-        console.log("File has been written");
-      });
-    });
-  });
+const server = http.createServer((req, res) => {
+  res.end("Hello from the server !");
 });
 
-console.log("Will read this file...");
+const PORT = 8000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
