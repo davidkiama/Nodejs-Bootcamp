8d054d5e2427cf2f9c1a05739c714d1cf0981bfd
davidkiama <davidkiama98@gmail.com>
2022-07-08 16:20:39 +0300

    Template building ofthe overview page

--- index.js
+++ index.js
@@ -4,8 +4,31 @@ const fs = require("fs");
 const { dirname } = require("path");
 
 //To read the file just once
+// We can read it synchronously since its top level code
+
+const templateCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8");
+const templateOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8");
+const templateProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, "utf-8");
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
+
+const replaceTemplate = (template, product) => {
+  let output = template;
+
+  output = output.replaceAll("{%PRODUCTNAME%}", product.productName);
+  output = output.replaceAll("{%IMAGE%}", product.image);
+  output = output.replaceAll("{%QUANTITY%}", product.quantity);
+  output = output.replaceAll("{%PRICE%}", product.price);
+  output = output.replaceAll("{%FROM%}", product.from);
+  output = output.replaceAll("{%NUTRIENTS%}", product.nutrients);
+  output = output.replaceAll("{%DESCRIPTION%}", product.description);
+  output = output.replaceAll("{%ID%}", product.id);
+
+  if (!product.organic) output = output.replaceAll("{%NOT_ORGANIC%}", "not-organic");
+
+  return output;
+};
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
@@ -13,9 +36,13 @@ const server = http.createServer((req, res) => {
   switch (pathName) {
     case "/":
     case "/overview":
-      res.end("This is the OVERVIEW");
+      res.writeHead(200, { "Content-type": "text/html" });
+
+      const cardsHtml = dataObj.map((el) => replaceTemplate(templateCard, el)).join("");
+      const output = templateOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+      res.end(output);
       break;
-    case "/kiama":
+    case "/product":
       res.end("This is KIAMA");
       break;
     case "/api":
